7988531469513ec10d49fac494b3e5c52cdd1bd7
panjunxun <jxpan@stu.xidian.edu.cn>
2022-01-18 23:35:49 +0800

    Web Day 4 实现分组支持

--- gee/gee.go
+++ gee/gee.go
@@ -7,8 +7,39 @@ import (
 
 type HandlerFunc func(ctx *Context)
 
+type RouterGroup struct {
+	prefix     string
+	middleware []HandlerFunc // 这个暂时用不到
+	parent     *RouterGroup  // 如果 engine 全部存储的话，parent 就没什么作用了
+	engine     *Engine
+}
+
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
+}
+
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := g.engine
+	newGroup := &RouterGroup{
+		prefix:     g.prefix + prefix,
+		middleware: make([]HandlerFunc, 0),
+		parent:     g,
+		engine:     g.engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (g *RouterGroup) GET(pattern string, f HandlerFunc) {
+	pattern = g.prefix + pattern
+	g.engine.addRoute("GET", pattern, f)
+}
+
+func (g *RouterGroup) POST(pattern string, f HandlerFunc) {
+	pattern = g.prefix + pattern
+	g.engine.addRoute("POST", pattern, f)
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -16,12 +47,8 @@ func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	e.router.handle(ctx)
 }
 
-func (e *Engine) GET(pattern string, f HandlerFunc) {
-	e.router.addRoute("GET", pattern, f)
-}
-
-func (e *Engine) POST(pattern string, f HandlerFunc) {
-	e.router.addRoute("POST", pattern, f)
+func (e *Engine) addRoute(method string, pattern string, f HandlerFunc) {
+	e.router.addRoute(method, pattern, f)
 }
 
 func (e *Engine) Run(addr string) error {
@@ -30,5 +57,8 @@ func (e *Engine) Run(addr string) error {
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
